f1cc900ec0a66e1ea782d71512bda82d092fa10f
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-24 19:46:59 +0200

    tr

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
 const app = express();
-const port = 3000;
-const mongoURI = 'mongodb+srv://mouadchiali:mouadchiali@clustertestprojet.n7r4egf.mongodb.net/docbpm';
+const port = process.env.PORT ||3000;
 
-// Define schema for data
-const dataSchema = new mongoose.Schema({
-  heartbeat: Number
-});
-
-// Define model for data
-const Data = mongoose.model('Data', dataSchema);
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('Error connecting to MongoDB:', error));
-
-// Route to handle POST requests for inserting data
-app.post('/api/data', async (req, res) => {
-  try {
-    const data = req.body;
-    await Data.create(data);
-    res.status(201).send('Data inserted into MongoDB');
-  } catch (error) {
-    console.error('Error inserting data into MongoDB:', error);
-    res.status(500).send('Internal server error');
-  }
-});
-
-// Route to handle GET requests for fetching data
-app.get('/api/data', async (req, res) => {
-  try {
-    const data = await Data.find();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error fetching data from MongoDB:', error);
-    res.status(500).send('Internal server error');
-  }
+app.post('/heart-rate', (req, res) => {
+  const heartRate = req.body.heartRate;
+  console.log('Received heart rate:', heartRate);
+  // Here you can do further processing or send it to clients connected via WebSocket
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
