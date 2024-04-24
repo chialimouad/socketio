f7765b52bdb55df0f644dd418a117881dec9d8e8
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-25 00:56:32 +0200

    lp

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
-const port = process.env.PORT ||3000;
+const PORT = 3000;
+const mongoUri = 'mongodb+srv://mouadchiali:mouadchiali@clustertestprojet.n7r4egf.mongodb.net/bp';
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/heart-rate', (req, res) => {
-  const heartRate = req.body.heartRate;
-  console.log('Received heart rate:', heartRate);
-  // Here you can do further processing or send it to clients connected via WebSocket
-  res.sendStatus(200);
+// Handle POST requests to /data
+app.post('/data', async (req, res) => {
+  const data = req.body;
+
+  // Connect to MongoDB Atlas
+  const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    await client.connect();
+
+    // Insert data into MongoDB
+    const db = client.db('<dbname>');
+    const collection = db.collection('sensor_data');
+    await collection.insertOne(data);
+
+    res.status(200).send('Data received and stored successfully');
+  } catch (error) {
+    console.error('Error storing data:', error);
+    res.status(500).send('Error storing data');
+  } finally {
+    await client.close();
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
